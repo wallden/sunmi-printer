2aeb846f814d1c0e1a21a55b9dc291e6fb90f8c5
Henrik Walldén <hwallden@gmail.com>
2023-03-06 19:56:47 +0100

    Initial commit

---
+++ android/src/main/java/se/pubq/sunmiprinting/SunmiPrinter.java
@@ -0,0 +1,11 @@
+package se.pubq.sunmiprinting;
+
+import android.util.Log;
+
+public class SunmiPrinter {
+
+    public String echo(String value) {
+        Log.i("Echo", value);
+        return value;
+    }
+}
